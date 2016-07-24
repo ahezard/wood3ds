780beeaf25c323d593eaf50d57389b8769bc76ff
ahezard <ahezard@gmail.com>
2016-07-24 17:33:27 +0200

    produce nds for no$gba

--- build_cia.sh
+++ build_cia.sh
@@ -1,9 +1,12 @@
 cp akmenu4/akmenu4_r4.nds akmenu4_r4_dsi.nds
+cp akmenu4/akmenu4_r4.nds akmenu4_r4_dsi_nogba.nds
 
 python patch_ndsheader_dsiware.py --read akmenu4_r4_dsi.nds > akmenu4_r4.nds_header.txt
 
 python patch_ndsheader_dsiware.py --mode dsi --title "AKMENU4" --maker 01 --code KAKE akmenu4_r4_dsi.nds
 
 python patch_ndsheader_dsiware.py --read akmenu4_r4_dsi.nds > akmenu4_r4_dsi.nds_header_patched.txt
+
+python patch_ndsheader_dsiware.py --mode dsinogba --title "AKMENU4" --maker 01 --code KAKE akmenu4_r4_dsi_nogba.nds
 
 ./make_cia --srl=akmenu4_r4_dsi.nds
